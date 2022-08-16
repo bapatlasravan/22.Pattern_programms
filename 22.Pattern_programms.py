ccc6d417275e653dd2ecf90e0d5a6ff5a0ba97cd
BAPATLA SRAVAN <103932090+bapatlasravan@users.noreply.github.com>
2022-08-16 10:17:20 +0530

    Add files via upload

---
+++ 22.Pattern_programms.py
@@ -0,0 +1,24 @@
+# program 1:-
+"""
+rows=int(input("enter number of rows: "))
+for i in range(0,rows):
+    for i in range(0,i+1):
+        print("*",end="")
+    print("\r")
+"""
+# program 2:-
+"""
+rows=int(input("enter number of rows: "))
+for i in range(rows+1,0,-1):
+    for i in range(0,i-1):
+        print("*",end="")
+    print("\r")
+"""
+# program 3:-
+"""
+rows=int(input("enter number of rows: "))
+for i in range(rows):
+    for j in range(rows):
+        print("*",end="")
+    print("\r")
+"""
